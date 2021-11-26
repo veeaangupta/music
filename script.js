6c7292b7d8f035aaf3059833d5ee5e59869a4147
veeaangupta <95080000+veeaangupta@users.noreply.github.com>
2021-11-26 17:10:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function j(){
+ 
+  document.getElementById('j').play()
+
+}
+
+function t(){
+  document.getElementById('t').play()
+}
+
+function l(){
+  document.getElementById('l').play()
+}
+
+function volume(){
+  document.getElementById('j').volume=document.getElementById('volume').value
+
+  document.getElementById('t').volume=document.getElementById('volume').value
+
+
+  document.getElementById('l').volume=document.getElementById('volume').value
+}
